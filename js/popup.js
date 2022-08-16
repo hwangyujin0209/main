2f5c394a94e8062632531076198f60ef4ba59370
hwangyujin0209 <111342409+hwangyujin0209@users.noreply.github.com>
2022-08-16 16:29:41 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,35 @@
+$(function(){
+  $('.p_box1').click(function(){
+    $('.popup_1').fadeIn();
+  });
+
+  $('.popup_1 p').click(function(){
+    $('.popup_1').fadeOut();
+  });
+
+  // 
+  $('.p_box2').click(function(){
+    $('.popup_2').fadeIn();
+  });
+
+  $('.popup_2 p').click(function(){
+    $('.popup_2').fadeOut();
+  });
+  // 
+  $('.p_box3').click(function(){
+    $('.popup_3').fadeIn();
+  });
+
+  $('.popup_3 p').click(function(){
+    $('.popup_3').fadeOut();
+  });
+  //
+  $('.p_box4').click(function(){
+    $('.popup_4').fadeIn();
+  });
+
+  $('.popup_4 p').click(function(){
+    $('.popup_4').fadeOut();
+  });
+
+});
